389b58d198a457ceb9c2cf27f4ace6da291c857e
Jack Martin <jackmartin.dev@gmail.com>
2024-10-03 12:41:12 +0900

    Add movie data model

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"movies.jackmartin.net/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +19,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		Title:     "Shrek 2",
+		Runtime:   93,
+		Genres:    []string{"action", "comedy"},
+		Version:   1,
+		CreatedAt: time.Now(),
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
