05999b5dee3096862d3709c089187f8b746ac754
Jack Martin <jackmartin.dev@gmail.com>
2024-10-14 12:51:45 +0900

    Refactor movie DB

--- migrations/000001_create_movies_table.up.sql
+++ migrations/000001_create_movies_table.up.sql
@@ -2,8 +2,12 @@ CREATE TABLE IF NOT EXISTS movies (
     id bigserial PRIMARY KEY,  
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     title text NOT NULL,
-    year integer NOT NULL,
-    runtime integer NOT NULL,
+    overview text NOT NULL,
+    language text NOT NULL,
+    release_date date NOT NULL,
+    rating float NOT NULL,
+    poster_url text NOT NULL,
+    backdrop_url text NOT NULL,
     genres text[] NOT NULL,
     version integer NOT NULL DEFAULT 1
 );
