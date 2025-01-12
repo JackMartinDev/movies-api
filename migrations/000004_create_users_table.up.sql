b456ad84e16ccb48daafff208503f0ff8bf1e4e7
Jack Martin <jackmartin.dev@gmail.com>
2025-01-12 14:50:47 +0900

    Update user table schema

--- migrations/000004_create_users_table.up.sql
+++ migrations/000004_create_users_table.up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
-    id bigserial PRIMARY KEY,  
-    username varchar(25) UNIQUE NOT NULL,
-    email varchar(100) UNIQUE NOT NULL,
-    password_hash text NOT NULL,
-    profile_url text NOT NULL,
-    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    name text NOT NULL,
+    email citext UNIQUE NOT NULL,
+    password_hash bytea NOT NULL,
+    activated bool NOT NULL,
+    version integer NOT NULL DEFAULT 1
 );
